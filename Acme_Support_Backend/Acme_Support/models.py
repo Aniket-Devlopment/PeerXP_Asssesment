0fdfdc9384cc42c5208a145c37717453f7370fd4
aniket karanjikar <karanjikaraniket@gmail.com>
2022-10-19 18:43:35 +0530

    Models Created

--- Acme_Support_Backend/Acme_Support/models.py
+++ Acme_Support_Backend/Acme_Support/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    Name = models.CharField(max_length=256)
+    Email = models.EmailField()
+    Phone_Number = models.IntegerField()
+    Password = models.CharField(max_length = 15)
+    Department = models.CharField(max_length = 2048)
+    Role = models.CharField(max_length = 256)
+    Created_By = models.CharField(max_length=256)
+    Created_At = models.DateTimeField()
+    Updated_At = models.DateTimeField()
+    class Meta:
+        db_table = "User"
+
+class Department(models.Model):
+    Name = models.CharField(max_length=256)
+    Description = models.CharField(max_length = 2048)
+    Created_By = models.CharField(max_length=256)
+    Created_At = models.DateTimeField()
+    Updated_At = models.DateTimeField()
+    
+    class Meta:
+        db_table = 'Department'
